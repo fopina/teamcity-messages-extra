c5f7a5b302e7b0d8ed87d7116ca5180546872cd0
Filipe Pina <hzlu1ot0@duck.com>
2023-11-09 08:00:50 +0000

    x

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,15 @@
 from unittest import TestCase
 import io
+import time
+from datetime import datetime
 
 from teamcity_extra import messages
+
+
+def fixed_time():
+    return fixed_time._time
+fixed_time._time = time.mktime(datetime(2000, 11, 2, 10, 23, 1).timetuple()) + 0.5569
+
 
 class Test(TestCase):
     def test_constructor(self):
@@ -9,9 +17,9 @@ class Test(TestCase):
 
     def test_metadata(self):
         out = io.BytesIO()
-        tsm = messages.TeamcityServiceMessages(output=out)
+        tsm = messages.TeamcityServiceMessages(output=out, now=fixed_time)
         tsm.testMetadata('testName', 'link', value='https://github.com', type='link')
         self.assertEqual(
             out.getvalue(),
-            b"##teamcity[testMetadata timestamp='2023[90 chars]']\n"
+            b"##teamcity[testMetadata timestamp='2000-11-02T10:23:01.556' name='link' testName='testName' type='link' value='https://github.com']\n"
         )
