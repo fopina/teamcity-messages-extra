9ac23cdef3a316874b2ef28a8e3dec7da1356e5a
Filipe Pina <63779195+fopinappb@users.noreply.github.com>
2024-07-25 10:34:16 +0100

    add new service messages: addBuildTag, removeBuildTag, buildStatisticValue

--- teamcity_extra/messages.py
+++ teamcity_extra/messages.py
@@ -5,3 +5,15 @@ class TeamcityServiceMessages(_TSM):
     def testMetadata(self, testName, name, value='', type='', flowId=None):
         # https://www.jetbrains.com/help/teamcity/reporting-test-metadata.html#Reporting+Additional+Test+Data
         self.message('testMetadata', name=name, testName=testName, value=value, type=type, flowId=flowId)
+
+    def buildStatisticValue(self, key, value):
+        # https://www.jetbrains.com/help/teamcity/service-messages.html#Reporting+Build+Statistics
+        self.message('buildStatisticValue', key=key, value=str(value))
+
+    def addBuildTag(self, tag):
+        # https://www.jetbrains.com/help/teamcity/service-messages.html#Adding+and+Removing+Build+Tags
+        self._single_value_message('addBuildTag', tag)
+
+    def removeBuildTag(self, tag):
+        # https://www.jetbrains.com/help/teamcity/service-messages.html#Adding+and+Removing+Build+Tags
+        self._single_value_message('removeBuildTag', tag)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -14,14 +14,40 @@ fixed_time._time = time.mktime(datetime(2000, 11, 2, 10, 23, 1).timetuple()) + 0
 
 
 class Test(TestCase):
-    def test_constructor(self):
+    def setUp(self) -> None:
+        super().setUp()
+        self.tsm_output = io.BytesIO()
+        self.tsm = messages.TeamcityServiceMessages(output=self.tsm_output, now=fixed_time)
+
+    def assertOutput(self, expected_output: str):
+        self.assertEqual(
+            self.tsm_output.getvalue(),
+            expected_output.encode(),
+        )
+
+    def test_default_constructor(self):
         messages.TeamcityServiceMessages()
 
-    def test_metadata(self):
-        out = io.BytesIO()
-        tsm = messages.TeamcityServiceMessages(output=out, now=fixed_time)
-        tsm.testMetadata('testName', 'link', value='https://github.com', type='link')
-        self.assertEqual(
-            out.getvalue(),
-            b"##teamcity[testMetadata timestamp='2000-11-02T10:23:01.556' name='link' testName='testName' type='link' value='https://github.com']\n",
+    def test_testMetadata(self):
+        self.tsm.testMetadata('testName', 'link', value='https://github.com', type='link')
+        self.assertOutput(
+            "##teamcity[testMetadata timestamp='2000-11-02T10:23:01.556' name='link' testName='testName' type='link' value='https://github.com']\n",
+        )
+    
+    def test_buildStatisticValue(self):
+        self.tsm.buildStatisticValue('myVal', '1')
+        self.assertOutput(
+            "##teamcity[buildStatisticValue timestamp='2000-11-02T10:23:01.556' key='myVal' value='1']\n",
+        )
+    
+    def test_addBuildTag(self):
+        self.tsm.addBuildTag('customTag')
+        self.assertOutput(
+            "##teamcity[addBuildTag 'customTag']\n",
+        )
+    
+    def test_removeBuildTag(self):
+        self.tsm.removeBuildTag('customTag')
+        self.assertOutput(
+            "##teamcity[removeBuildTag 'customTag']\n",
         )
